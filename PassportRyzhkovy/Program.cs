f50e2ed6d9bea5ab4924aecacc0bb795da68e876
Ryzhkovy <sgk-pr@mail.ru>
2020-04-10 10:57:53 +0300

    Добавьте файлы проекта.

---
+++ PassportRyzhkovy/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassportRyzhkovy
+{
+    class Monster
+    {
+        public int health;
+        public int ammo;
+        public string name;
+        
+        public Monster()
+        {
+            this.health = 100;
+            this.ammo = 100;
+            this.name = "Noname";
+        }
+        public Monster(string name) : this()
+        {
+            this.name = name;
+        }
+        public Monster(int health, int ammo, string name)
+        {
+            this.health = health;
+            this.ammo = ammo;
+            this.name = name;
+        }
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                if (value > 0)
+                    health = value;
+                
+                else
+                    health = 0;
+            }
+        }
+        public int Ammo
+        {
+            get
+            {
+                return ammo;
+            }
+            set
+            {
+                if (value > 0) ammo = value;
+                else
+                    ammo = 0;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        
+
+        public void Passport()
+        {
+            Console.WriteLine("Monster{0} \t health = {1} ammo = {2}",name,health,ammo);
+
+        }
+        
+       
+    }
+    class Class1
+    {
+        static void Main()
+        {
+            Monster Masha = new Monster(200,200,"Masha");
+            Masha.Passport();
+            --Masha.Health;
+            Masha.Ammo += 100;
+            Masha.Passport();
+            Console.Read();
+          
+        }
+    }
+    
+}
